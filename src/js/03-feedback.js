31a5e2197a484b2f544c0a303306f7d1db2c887e
Vasja Hajnas <129309015+VasjaHajnas@users.noreply.github.com>
2023-08-09 12:59:29 +0300

    thirt task#2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const saveFormState = throttle(() => {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+}, 500);
+
+emailInput.addEventListener('input', saveFormState);
+messageInput.addEventListener('input', saveFormState);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const savedState = localStorage.getItem('feedback-form-state');
+  if (savedState) {
+    const parsedState = JSON.parse(savedState);
+    emailInput.value = parsedState.email;
+    messageInput.value = parsedState.message;
+  }
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+  console.log('Form submitted with values:', {
+    email: emailInput.value,
+    message: messageInput.value,
+  });
+});
+//  .
