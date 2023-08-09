4d82796494a9dfd078f811735ae0ab89bd9a0607
Vasja Hajnas <129309015+VasjaHajnas@users.noreply.github.com>
2023-08-09 12:40:45 +0300

    second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Vimeo from '@vimeo/player';
 
+const vimeoPlayer = new Vimeo('vimeo-player');
+
+vimeoPlayer.on('timeupdate', event => {
+  const currentTime = event.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    vimeoPlayer.setCurrentTime(savedTime);
+  }
+});
